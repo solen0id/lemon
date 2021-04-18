d051698f5ce75d259ffae3f8cb3fae4800833273
Max Patzelt <max.patzelt@legislayer.com>
2021-04-18 14:17:05 +0200

    Create model "Order"

--- lemon/order/models.py
+++ lemon/order/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class SIDES(models.TextChoices):
+    BUY = "buy"
+    SELL = "sell"
+
+
+class Order(models.Model):
+    isin = models.CharField(max_length=12)
+    limit_price = models.DecimalField(null=True, max_digits=19, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    side = models.CharField(max_length=4, choices=SIDES.choices)
+    valid_until = models.PositiveIntegerField()  # 32-bit int, matches UNIX timestamp
